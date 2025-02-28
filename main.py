6a0a3a8e27069f5e83a7d0e4e273172342e2ae8e
Santiago Morales <60114568+SantiagoMorales1@users.noreply.github.com>
2025-02-27 23:28:05 -0500

    Cambion en con 14

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from app.routes.routes import router
-
+from app.services.service import get_conn_sql_service
 
 app = FastAPI(title="MI App - Globant Prueba")
 
 app.include_router(router)
 
-#if __name__ == "__main__":   
-#    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+if __name__ == "__main__":   
+    #uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    get_conn_sql_service()
